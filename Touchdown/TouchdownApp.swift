4cdd0db375325cac4f910c2cc0c3c44b6d125a09
corp00367 <lucascardoso@ext.cvccorp.com.br>
2021-01-29 16:09:17 -0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Lucas Cardoso on 29/01/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
